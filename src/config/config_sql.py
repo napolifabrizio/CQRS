ce30220b35214cdb28af8149467023480a5ad165
Fabrizio Napoli <fabrizionapoli50@gmail.com>
2024-10-30 08:22:06 -0300

    refactor: changing simple methods for class methods and puting parameters for dba configs

--- src/config/config_sql.py
+++ src/config/config_sql.py
@@ -1,6 +1,21 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Table, INTEGER, VARCHAR, FLOAT
+from sqlalchemy.ext.declarative import declarative_base
 
 link_database = os.environ.get('LINK_DATABASE_SQL', "must_be_set_in_env")
 
 engine = create_engine(link_database)
+
+Base = declarative_base()
+
+TABLE_TRANSACTIONS = Table(
+    "transactions",
+    Base.metadata,
+    Column("codcli", INTEGER, primary_key=True),
+    Column("cpf", VARCHAR(30), nullable=False),
+    Column("name", VARCHAR(100), nullable=False),
+    Column("price", FLOAT, nullable=False),
+    Column("company", VARCHAR(50), nullable=False),
+    Column("paymentmethod", VARCHAR(20), nullable=False),
+    Column("product", VARCHAR(30), nullable=True)
+)
--- src/services/service.py
+++ src/services/service.py
@@ -1,29 +1,35 @@
-from repositories.repository_mongo import insert_one_mongo, delete_one_mongo, get_one_mongo, update_one_mongo
-from repositories.repository_sql import insert_sql, update_sql, delete_sql, select_sql
+from repositories.repository_mongo import MongoRepo
+from repositories.repository_sql import RepoSql
 from models.Transaction import Transaction
 
-def add_document(document: Transaction):
-    if get_one_mongo(document.CodCli):
-        print("The account already exists")
-        return False
-    document_json = document.model_dump()
-    insert_one_mongo(document_json)
-    insert_sql(document_json)
-    return True
+class Service():
 
-def delete_document(codcli: int):
-    if get_one_mongo(codcli):
-        delete_one_mongo(codcli)
-        delete_sql(codcli)
+    def __init__(self, repo_mongo: MongoRepo, repo_sql: RepoSql):
+        self._repoMongo = repo_mongo
+        self._repoSql = repo_sql
+
+    def add_document(self, document: Transaction):
+        if self._repoMongo.get_one_mongo(document.CodCli):
+            print("The account already exists")
+            return False
+        document_json = document.model_dump()
+        self._repoMongo.insert_one_mongo(document_json)
+        self._repoSql.insert_sql(document_json)
         return True
-    print("Account nof found")
-    return False
 
-def update_document(new_valor: dict, document: Transaction):
-    update_one_mongo(document.CodCli, {"$set": new_valor})
-    update_sql(document.model_dump())
-    return True
+    def delete_document(self, codcli: int):
+        if self._repoMongo.get_one_mongo(codcli):
+            self._repoMongo.delete_one_mongo(codcli)
+            self._repoSql.delete_sql(codcli)
+            return True
+        print("Account nof found")
+        return False
 
-def get_document(codcli: int):
-    res = select_sql(codcli)
-    return res
+    def update_document(self, new_valor: dict, document: Transaction):
+        self._repoMongo.update_one_mongo(document.CodCli, {"$set": new_valor})
+        self._repoSql.update_sql(document.model_dump())
+        return True
+
+    def get_document(self, codcli: int):
+        res = self._repoSql.select_sql(codcli)
+        return res
